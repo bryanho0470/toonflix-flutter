68f0beb2d078246fd4b764d1a09b0973abb28632
bryanho0470 <bryanho0470@gmail.com>
2023-11-24 13:28:12 +0900

    time format setting

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,14 +10,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const twerntyFiveMinutes = 1500;
+  int totalSeconds = twerntyFiveMinutes;
+  int totalPomodoros = 0;
   bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
-    setState(() {
-      totalSeconds = totalSeconds - 1;
-    });
+    if (totalSeconds == 0) {
+      setState(
+        () {
+          totalPomodoros = totalPomodoros + 1;
+          isRunning = false;
+          totalSeconds = twerntyFiveMinutes;
+        },
+      );
+      timer.cancel();
+    } else {
+      setState(
+        () {
+          totalSeconds = totalSeconds - 1;
+        },
+      );
+    }
   }
 
   void onStartPressed() {
@@ -37,6 +52,11 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "$totalSeconds",
+                format(totalSeconds),
                 style: const TextStyle(
                   color: Colors.white,
                   fontSize: 90,
@@ -96,7 +116,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   .color),
                         ),
                         Text(
-                          "0",
+                          "$totalPomodoros",
                           style: TextStyle(
                               fontSize: 60,
                               fontWeight: FontWeight.bold,
